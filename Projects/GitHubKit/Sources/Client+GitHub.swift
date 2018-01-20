73f6980ace9adf5abd7e3d0e58a45343222b12bc
Pedro Piñera <pepibumur@gmail.com>
2018-01-20 13:54:23 -0500

    Disable API client cache

--- Projects/GitHubKit/Sources/Client+GitHub.swift
+++ Projects/GitHubKit/Sources/Client+GitHub.swift
@@ -9,8 +9,12 @@ public extension Client {
     /// - Returns: GitHub client.
     public static func github(accessToken: @escaping () -> String?) -> Client {
         let urlComponents = URLComponents(string: "https://api.github.com")!
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .reloadIgnoringLocalCacheData
+        config.urlCache = nil
+        let session = URLSession.init(configuration: config)
         return Client(baseURLComponents: urlComponents,
-                      session: URLSession.shared) { request in
+                      session: session) { request in
                         var mutableRequest = request
                         if mutableRequest.allHTTPHeaderFields == nil  { mutableRequest.allHTTPHeaderFields = [:] }
                         mutableRequest.allHTTPHeaderFields?["Content-Type"] = "application/json"
